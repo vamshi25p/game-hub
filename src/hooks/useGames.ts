80957cf4840f0abdab90ec1ef7555a81b26c2da0
Vamshi Krishna <vamshikrishna788sai@gmail.com>
2024-04-29 17:36:47 +0530

    Displaying platform icons.

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,11 +1,16 @@
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
-
+export interface Platform {
+  id: number;
+  slug: string;
+  name: string;
+}
 export interface Game {
   id: number;
   name: string;
   background_image: string;
+  parent_platforms: { platform: Platform }[];
 }
 
 interface FetchGamesResponse {
@@ -19,7 +24,7 @@ const useGames = () => {
   useEffect(() => {
     const controller = new AbortController();
     apiClient
-      .get<FetchGamesResponse>("/xgames", { signal: controller.signal })
+      .get<FetchGamesResponse>("/games", { signal: controller.signal })
       .then((res) => setGames(res.data.results))
       .catch((err) => {
         if (err instanceof CanceledError) return;
